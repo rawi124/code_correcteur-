44802babfc687c8f5b02d0b5edd7993d02dec160
rawi124 <rawia.ben.amira1997@gmail.com>
2022-03-28 12:25:05 +0200

    nouvelle version

--- code_correcteur--master/codes_correcteur.c
+++ code_correcteur--master/codes_correcteur.c
@@ -14,6 +14,18 @@ int* conversion(int octet)
   return bit ;
 }
 
+int* conversion2(int octet)
+{
+  int copie = octet ;
+  int * bit = malloc(8*sizeof(int));
+  for(int j = 0 ; j < 8 ; j++)
+  {
+    bit[j] = copie & 1;
+    printf("%d ",bit[j]);
+    copie = copie >> 1 ;
+  }
+  return bit ;
+}
 
 int bruite(int octet, float p , int i)
 {
@@ -91,13 +103,13 @@ unsigned int hamming(int v1, int v2)
 }
 int weight(int x)
 {
-    int res = 0 ;
-    while(x > 0)
-    {
-        x&=(x-1);
-        res++;
-    }
-    return res ;
+	int res = 0 ;
+	while(x > 0)
+	{
+		x&=(x-1);
+		res++;
+	}
+	return res ;
 }
 unsigned int hamming2(int v1, int v2)
 {
@@ -107,80 +119,14 @@ unsigned int hamming2(int v1, int v2)
   return weight(xo);
 
 }
-int* vecteur(int val)
-{
-  int copie = val ;
-  int * bit = malloc(8*sizeof(int));
-  for(int j = 7 ; j >= 0 ; j--)
-  {
-    bit[j] = copie & 1;
-    copie = copie >> 1 ;
-  }
-  for(int j = 0 ; j < 8 ; j++)
-  {
-  printf("%d ",bit[j]);
-  }
-
-
-  return bit ;
-}
-int valeur(int* vect)
-{
-  int nv = vect[7] ;
-  int cpt = 2 ;
-  for(int t =  6 ; t >=0 ; t--)
-  {
-    nv = nv + vect[t]*cpt ;
-    cpt = cpt * 2 ;
-  }
-  printf(" %d\n",nv);
-  return nv ;
-}
-int* encode(int ** g, int* v){
-  int * l = malloc(6*sizeof(int));
-  for(int j = 0 ; j < 3 ; j++)
-  {
-      int s = 0;
-      for(int z = 0 ; z < 2 ; z++)
-     s = s + (g[z][j]*v[z]);
-      printf("%d\n",s);
-      l[j] = s;
-    }
-    return l;
-}
-int* controle(int ** h, int* c){
-  int * l = malloc(6*sizeof(int));
-  for(int i = 0 ; i < 2 ; i++)
-  {
-      int s = 0;
-      for(int j = 0 ; j < 6 ; j++)
-     s = s + (h[i][j]*c[j]);
-      printf("%d\n",s);
-      l[i] = s;
-    }
-    return l;
-}
 int main(int argc, char*argv[])
 {
-
   srand(time(NULL));
- 
-  int ** tab = calloc(2,sizeof(int*));
-  for(int i = 0 ; i < 2 ; i++)
-      tab[i] = calloc(3,sizeof(int));
+  //bruite(45,0.9,2);
+  // copie(argv[1],argv[2]);
+  // int x = hamming(10,4);
+  // printf("%d\n",x);
+  //conversion2(5);
+  hamming2(1,2);
 
-  tab[0][0] = 3 ;
-  tab[0][1] = 4 ;
-  tab[0][2] = 7 ;
-  tab[1][0] = 5 ;
-  tab[1][1] = 6 ;
-  tab[1][2] = 8 ;
- 
-  int *t = calloc(3,sizeof(int));
-  t[0] = 1;
-  t[1] = 3;
-  t[2] = 4;
-  controle(tab,t);
- 
-  int x = valeur(vecteur(9));
 }
